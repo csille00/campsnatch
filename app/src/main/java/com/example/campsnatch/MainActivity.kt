a6774ae958e5fd3e7aacdd53af991d0d6c55b458
Logan Orr <logan_orr@icloud.com>
2023-08-04 22:13:28 -0600

    Getting Push Notification

--- app/src/main/java/com/example/campsnatch/MainActivity.kt
+++ app/src/main/java/com/example/campsnatch/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.campsnatch.databinding.ActivityMainBinding
 import com.example.campsnatch.support.CampsnatchActivity
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : CampsnatchActivity() {
 
@@ -13,5 +14,25 @@ class MainActivity : CampsnatchActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Request FCM token
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                val token = task.result
+                // Here, you have the FCM registration token
+                // You can now send this token to your server to identify the device
+                // and send push notifications to it.
+
+                // For testing purposes, you can log the token to the console
+                token?.let {
+                    println("FCM Registration Token: $it")
+                }
+            } else {
+                // Failed to get the FCM token
+                println("Failed to get FCM registration token: ${task.exception}")
+            }
+        }
     }
+
+
 }
